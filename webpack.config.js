7c3601cb0cbad19df39f5f024d69ea308fe2012f
Sharon Desmond <mathemagiks@gmail.com>
2018-01-09 19:03:39 +0000

    Set up react environment by installing babel dependencies and configure webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-	entry:'./source/client.js',
+	entry:['babel-polyfill','./source/client.js'],
 	output:{
 		path:"./",
 		filename:'index.js'
@@ -8,5 +8,12 @@ module.exports = {
  	inline:true,
  	contentBase:'./public',
  	port:3000
- }
+ },
+ module:{
+ 	loaders: [
+ 	{test:/\.js$/,
+ 	 exclude:/node_modules/,
+ 	  loader:'babel'}
+ 	]
+ },
 };
