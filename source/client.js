9db3270ccc48bd79cb5164388f3f6b6b537f9041
Sharon Desmond <mathemagiks@gmail.com>
2018-01-10 15:36:51 +0000

    Configure webpack.config.js and install a compatiable babel-core version. Skeleton of webpage ok.

--- source/client.js
+++ source/client.js
@@ -1,1 +1,12 @@
-console.log('hello000');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const component = <div className="jumbotron jumbotron-fulid text-md-center" style={{backgroundImage:'url(img/2s.jpg)',color:'white'}}>
+			<div className="container">
+				<h1>Building React.js User Interfaces</h1>
+				<p>with Bootstrap and SASS.</p>
+				<p><a className="btn btn-primary" href="http://www.sharondesmond.com" target="_blank">Discover things.</a></p>
+			</div>
+		</div>;
+
+ReactDOM.render(component,document.getElementById('react'));
