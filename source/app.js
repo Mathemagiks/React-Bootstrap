585aa5718a4a4477059206459d3bc647a9943cfa
Sharon Desmond <mathemagiks@gmail.com>
2018-01-10 18:59:23 +0000

    Create React Bootstrap button

--- source/app.js
+++ source/app.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import Button from './bootstrap/button';
 
 export default class App extends React.Component{
 	render(){
-		return <div className="jumbotron jumbotron-fulid text-md-center" style={{backgroundImage:'url(img/2s.jpg)',color:'white'}}>
+		return <div className="jumbotron jumbotron-fluid text-md-center" style={{backgroundImage:'url(img/2s.jpg)',color:'white'}}>
 			<div className="container">
 				<h1>Building React.js User Interfaces</h1>
 				<p>with Bootstrap and SASS.</p>
-				<p><a className="btn btn-primary" href="http://www.sharondesmond.com" target="_blank">Discover things.</a></p>
+				<p><Button className="btn btn-primary" href="http://www.sharondesmond.com" target="_blank"/></p>
 			</div>
 		</div>;
 
--- source/client.js
+++ source/client.js
@@ -2,6 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
 
-const component = 
 
-ReactDOM.render(<App/>,document.getElementById('react'));
+ReactDOM.render(<App />,document.getElementById('react'));
