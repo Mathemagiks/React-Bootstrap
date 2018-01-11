d84184a8356e57e7a14f9205fbd48454e96ea133
Sharon Desmond <mathemagiks@gmail.com>
2018-01-11 21:53:30 +0000

    Create the Junbotron Component.

--- source/app.js
+++ source/app.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import Button from './bootstrap/button';
+import Jumbotron from './bootstrap/jumbotron';
 
 export default class App extends React.Component{
 	render(){
-		return <div className="jumbotron jumbotron-fluid text-md-center" style={{backgroundImage:'url(img/2s.jpg)',color:'white'}}>
+		return <Jumbotron className="-fluid text-md-center" style={{backgroundImage:'url(img/2s.jpg)',color:'white'}}>
 			<div className="container">
 				<h1>Building React.js User Interfaces</h1>
 				<p>with Bootstrap and SASS.</p>
 				<p><Button className=" -dark" href="http://www.sharondesmond.com" target="_blank"/>
 				   <Button className=" -outline-light -lg -block" disabled/></p>
 			</div>
-		</div>;
+		</Jumbotron>;
 
 	}
 
