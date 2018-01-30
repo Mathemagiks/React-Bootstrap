67e6403f1cf9bf53de0f8d3b47d833dc4b26713b
Sharon Desmond <mathemagiks@gmail.com>
2018-01-30 18:17:26 +0000

    Animate in react with add-ons- npm install react-addons-css-transition-group

--- source/app.js
+++ source/app.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import Button from './bootstrap/button';
 import Jumbotron from './bootstrap/jumbotron';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 import './appsource.scss';
 
 export default class App extends React.Component{
 	render(){
-		return <Jumbotron className="-fluid text-sm-center custom" containerFluid={true}>
+		return <ReactCSSTransitionGroup transitionName="app" transitionAppear={true} transitionAppearTimeout={500}><Jumbotron className="-fluid text-sm-center custom" containerFluid={true}>
 				<h1>Building React.js User Interfaces</h1>
 				<p>with Bootstrap and SASS.</p>
 				<p><Button className=" -danger" href="http://www.sharondesmond.com" target="_blank"/>
 				   <Button className=" -outline-light -lg -block" disabled/></p>
-		</Jumbotron>;
+		</Jumbotron></ReactCSSTransitionGroup>;
 
 	}
 
